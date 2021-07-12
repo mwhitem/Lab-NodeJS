11dc9cb262c9b13f53f7dc16e3b9d4e4cbec6394
Madison White <mvwhite32@gmail.com>
2021-07-12 09:44:15 -0500

    commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+
+var path = require('path');
+var fs = require('fs');
+
+
+let chirps = [{
+    username: "bob",
+    chirpText: "hi"
+},
+{
+    username: "tom",
+    chirpText: "hello"
+},
+{
+    username: "jan",
+    chirpText: "bonjour"
+},
+{
+username: "ben",
+chirpText: "aloha"
+},
+{
+    username: "zac",
+    chirpText: "hola"
+}]
+
+
+// var dataPath = path.join(__dirname, '../data.json')
+
+// fs.readFile(path.join(__dirname, 'data.json' ), function(err, data) {
+// if(err) return console.log(err);
+// console.log(data.toString());
+// });
